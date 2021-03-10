e7f5bec2970e53ee817975b85bf802a62ffaf122
UladzislauKutarkin <vkutarkin@gmail.com>
2021-03-10 12:38:04 +0300

    feat: add i18 translate

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import { initReactI18next } from 'react-i18next'
+
+i18n
+    // Подключение бэкенда i18next
+    .use(Backend)
+    // Автоматическое определение языка
+    // модуль инициализации
+    .use (initReactI18next)
+    .init({
+        // Стандартный язык
+        fallbackLng: 'en',
+        debug: true,
+        // Распознавание и кэширование языковых кук
+        detection: {
+            order: ['queryString', 'cookie'],
+            cache: ['cookie']
+        },
+        interpolation: {
+            escapeValue: false
+        }
+    })
+
+export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
+import './i18n';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Suspense fallback={<div>Loading...</div>}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Suspense>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
