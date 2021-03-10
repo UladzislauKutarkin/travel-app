63955c6f2bc80517d90b77541731545608968d76
YA <vector_cp@mail.ru>
2021-03-10 19:14:29 +0300

    refactor: Homepage component to class

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,20 +2,27 @@ import Card from "../../components/Card";
 import "./HomePage.scss";
 import countries from "../../data/counties";
 import Header from "../../components/Header/Header";
+import { Component } from "react";
 
-const HomePage = (props) => {
-  console.log(props);
+class HomePage extends Component {
 
-  return (
-    <div className="container-fluid">
-      <Header />
-      <div className="cards-container">
-        {countries.map((country) => {
-          return <Card key={country.id} {...country} />;
-        })}
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header />
+        <div className="cards-container">
+          {countries.map((country) => {
+            return <Card key={country.id} {...country} />;
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 export default HomePage;
