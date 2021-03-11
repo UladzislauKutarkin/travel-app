492071cefd069e0e36d0b0abb6f3d0b3e58b74db
AlekseyGrimm <stalpetc@gmail.com>
2021-03-11 13:10:22 +0300

    feat: add new card

--- src/data/counties.ts
+++ src/data/counties.ts
@@ -3,17 +3,42 @@ const countries: any[] = [
     capital: 'Minsk',
     promo_photo_url: '',
     id: 1
-},
-{country: 'Ukraine',
-capital: 'Kiev',
-promo_photo_url: '',
-id: 2
-},
-{country: 'Usa',
-capital: 'New-York',
-promo_photo_url: '',
-id: 3
-}
+    },
+    {country: 'Ukraine',
+    capital: 'Kiev',
+    promo_photo_url: '',
+    id: 2
+    },
+    {country: 'Usa',
+    capital: 'New-York',
+    promo_photo_url: '',
+    id: 3
+    },
+    {country: 'United Kingdom',
+    capital: 'London',
+    promo_photo_url: '',
+    id: 4
+    },
+    {country: 'Australia',
+    capital: 'Canberra',
+    promo_photo_url: '',
+    id: 5
+    },
+    {country: 'France',
+    capital: 'Paris',
+    promo_photo_url: '',
+    id: 6
+    },
+    {country: 'Canada',
+    capital: 'Ottawa',
+    promo_photo_url: '',
+    id: 7
+    },
+    {country: 'United Arab Emirates',
+    capital: 'Abu-Dhabi',
+    promo_photo_url: '',
+    id: 8
+    },
 ];
 
 export default countries;
